cdabd052e75b891c112dfa957d0156c5da872e99
Márton Salomváry <salomvary@gmail.com>
2020-03-16 18:32:31 +0100

    Updates

---
+++ precache-manifest.e9c85b9675345fecc61a331293bc5701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadd127c71106339749983015885c6db",
+    "url": "/react-charts-demo/index.html"
+  },
+  {
+    "revision": "65e0b9df69ab4ddd2590",
+    "url": "/react-charts-demo/static/css/main.b84ce94a.chunk.css"
+  },
+  {
+    "revision": "a206f4c8740a4c6fef4a",
+    "url": "/react-charts-demo/static/js/2.7fdaf11c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-charts-demo/static/js/2.7fdaf11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e0b9df69ab4ddd2590",
+    "url": "/react-charts-demo/static/js/main.d1bca9ba.chunk.js"
+  },
+  {
+    "revision": "03c028e3817d51e46cdb",
+    "url": "/react-charts-demo/static/js/runtime-main.93d2ca69.js"
+  }
+]);
